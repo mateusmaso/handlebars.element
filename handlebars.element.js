ac3b5cd09f0695bf4771e692051a3e5bdc65a616
Mateus Maso <m.maso25@gmail.com>
2013-12-11 23:32:00 -0800

    Created handlebars.element.js

---
+++ handlebars.element.js
@@ -0,0 +1,134 @@
+(function(Handlebars) {
+
+  var store = {};
+  var increment = 0;
+  var escapeExpression = Handlebars.Utils.escapeExpression;
+
+  var isObject = function(object) {
+    return object === Object(object);
+  };
+
+  var isArray = function(object) {
+    return toString.call(object) == '[object Array]';
+  };
+
+  var flatten = function(array, flattenArray) {
+    flattenArray = flattenArray || [];
+
+    for (var index = 0; index < array.length; index++) {
+      if (isArray(array[index])) {
+        flatten(array[index], flattenArray);
+      } else {
+        flattenArray.push(array[index]);
+      }
+    };
+
+    return flattenArray;
+  };
+
+  var camelize = function(string){
+    return string.trim().replace(/[-_\s]+(.)?/g, function(match, word) { 
+      return word ? word.toUpperCase() : ""; 
+    });
+  };
+
+  var parse = function(value) {
+    try {
+      return JSON.parse(value);
+    } catch (error) {
+      return value;
+    }
+  };
+
+  var replaceWith = function(node, nodes) {
+    nodes = isArray(nodes) ? nodes : [nodes];
+
+    for (var index = 0; index < nodes.length; index++) {
+      if (index == 0) {
+        node.parentNode.replaceChild(nodes[index], node);
+      } else {
+        insertAfter(nodes[index - 1], nodes[index]);
+      }
+    }
+  };
+
+  var insertAfter = function(node, nodes) {
+    nodes = isArray(nodes) ? nodes : [nodes];
+    nodes.unshift(node);
+
+    for (var index = 1; index < nodes.length; index++) {
+      if (nodes[index - 1].nextSibling) {
+        nodes[index - 1].parentNode.insertBefore(nodes[index], nodes[index - 1].nextSibling);
+      } else {
+        nodes[index - 1].parentNode.appendChild(nodes[index]);
+      }
+    }
+  };
+
+  Handlebars.elements = {};
+
+  Handlebars.Utils.escapeExpression = function(value) {
+    if (isObject(value) && !(value instanceof Handlebars.SafeString)) {
+      var id = ++increment + "c";
+      store[id] = value;
+      value = id;
+    }
+
+    return escapeExpression.apply(this, [value]);
+  };
+
+  Handlebars.registerElement = function(name, fn, options) {
+    fn.options = options || {};
+    this.elements[name] = fn;
+  };
+
+  Handlebars.parseHTML = function(html) {
+    var div = document.createElement('div');
+    div.innerHTML = html.trim();
+
+    var nodes = flatten(div.childNodes);
+    var elements = [];
+
+    while (nodes.length != 0) {
+      var nextNodes = [];
+
+      for (var index = 0; index < nodes.length; index++) {
+        var childNodes = flatten(nodes[index].childNodes);
+
+        for (var bIndex = 0; bIndex < childNodes.length; bIndex++) {
+          nextNodes.push(childNodes[bIndex]);
+        }
+
+        if (/^hb-/i.test(nodes[index].nodeName)) {
+          elements.push(nodes[index]);
+        }
+      }
+
+      nodes = nextNodes;
+    }
+
+    for (var index = 0; index < elements.length; index++) {
+      var attributes = {};
+      var element = elements[index];
+      var elementName = element.tagName.toLowerCase().replace("hb-", "");
+      var fn = Handlebars.elements[elementName];
+
+      for (var bIndex = 0; bIndex < element.attributes.length; bIndex++) {
+        var attribute = element.attributes.item(bIndex);
+        var name = camelize(attribute.nodeName);
+        var value = store[attribute.nodeValue] || parse(attribute.nodeValue);
+        var bool = fn.options.booleans && fn.options.booleans.indexOf(name) >= 0;
+        delete store[attribute.nodeValue];
+
+        attributes[name] = bool ? value != false : value;
+      }
+
+      console.log(attributes)
+
+      replaceWith(element, fn.apply(element, [attributes]));
+    }
+
+    return flatten(div.childNodes);
+  };
+
+})(Handlebars);
