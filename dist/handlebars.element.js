e36cfeeff34bca4ea234be81292b9867ec15fcb4
Mateus Maso <m.maso25@gmail.com>
2017-05-24 01:47:54 -0300

    update dev dependencies + node engine >= 4.0.0

--- dist/handlebars.element.js
+++ dist/handlebars.element.js
@@ -1,11 +1,11 @@
 // handlebars.element
 // ------------------
-// v0.3.4
+// v0.3.5
 //
-// Copyright (c) 2013-2016 Mateus Maso
+// Copyright (c) 2013-2017 Mateus Maso
 // Distributed under MIT license
 //
-// http://github.com/mateusmaso/handlebars.element
+// 
 
 
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
